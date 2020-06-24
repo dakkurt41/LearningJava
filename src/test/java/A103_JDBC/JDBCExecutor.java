c8908cc48033e58d9d07f4492047910f56608942
Mehmet Dogan <m.dogan.4123@gmail.com>
2020-06-23 23:56:02 -0400

    database testing

--- src/test/java/A103_JDBC/JDBCExecutor.java
+++ src/test/java/A103_JDBC/JDBCExecutor.java
@@ -60,12 +60,12 @@ public class JDBCExecutor {
 
 
     @Test
-    public void Test3(){
+    public void CreateSqlTest(){
 
         try {
             Connection connection = dcm.getConnection();
             CustomerDAO customerDAO = new CustomerDAO(connection);
-            Customer customer = customerDAO.findById(1000);
+            Customer customer = customerDAO.findById(100);
             System.out.println(customer.getFirstName() + " " + customer.getLastName());
             customerDAO.create(customer);
             System.out.println("Database created.");
@@ -73,4 +73,71 @@ public class JDBCExecutor {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void UpdateSqlTest(){
+
+        try {
+            Connection connection = dcm.getConnection();
+            CustomerDAO customerDAO = new CustomerDAO(connection);
+            Customer customer = customerDAO.findById(1000);
+            System.out.println(customer.getFirstName() + " " + customer.getLastName() + " " + customer.getEmail());
+            customer.setFirstName("Tavsan");
+            customer.setLastName("Hanim");
+            customer.setEmail("tavsanhanim@wh.gov");
+            customer = customerDAO.update(customer);
+            System.out.println(customer.getFirstName() + " " + customer.getLastName() + " " + customer.getEmail());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void DeleteSqlTest(){
+
+        try {
+            Connection connection = dcm.getConnection();
+            CustomerDAO customerDAO = new CustomerDAO(connection);
+            Customer customer = new Customer();
+            customer.setFirstName("Kazim");
+            customer.setLastName("Karabekir");
+            customer.setEmail("george.washington@wh.gov");
+            customer.setPhone("(555) 555-6543");
+            customer.setAddress("1234 Main St");
+            customer.setCity("Sinan Tepesi");
+            customer.setState("VA");
+            customer.setZipCode("22121");
+            Customer dbcustomer = customerDAO.create(customer);
+            System.out.println("Create value : "+ dbcustomer);
+            dbcustomer = customerDAO.findById(dbcustomer.getId());
+            System.out.println("Get by id value : " + dbcustomer);
+
+            dbcustomer.setFirstName("Tavsan");
+            dbcustomer = customerDAO.update(dbcustomer);
+            System.out.println("Update value : " + dbcustomer);
+
+            customerDAO.delete(dbcustomer.getId());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Test
+    public void OrderLineTest(){
+
+        try{
+            Connection connection = dcm.getConnection();
+            OrderDAO orderDAO = new OrderDAO(connection);
+            Order order = orderDAO.findById(1002);
+            System.out.println(order);
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
