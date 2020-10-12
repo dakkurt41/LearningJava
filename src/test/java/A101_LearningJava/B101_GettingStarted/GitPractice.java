e4f5dcdfc60d7e777178adae3918df2d2cad6760
Mehmet Dogan <m.dogan.4123@gmail.com>
2020-10-12 18:36:39 -0400

    Commentler eklendi

--- src/test/java/A101_LearningJava/B101_GettingStarted/GitPractice.java
+++ src/test/java/A101_LearningJava/B101_GettingStarted/GitPractice.java
@@ -1,5 +1,7 @@
 package A101_LearningJava.B101_GettingStarted;
 
+import org.jcp.xml.dsig.internal.SignerOutputStream;
+
 public class GitPractice {
 
 
@@ -10,9 +12,21 @@ public class GitPractice {
 
     public static void main2(String[] args) {
         System.out.println(" git practice2 ");
+
+        // git status , anlik yapilian degisiklikleri takip etmek icin
+        System.out.println(" git status ");
+
+        // git add . butun dosyalari copy
         System.out.println(" git add . ");
+
+        // tracking who wrote, when code changes ,
         System.out.println(" git commit -m'message' " );
+
+        // git push mantigi paste
         System.out.println(" git push origin master ");
+
+        // lastest updateleri kendi bilgisayariniza yuklemek icin
+        System.out.println("git pull origin master ");
     }
 
 
