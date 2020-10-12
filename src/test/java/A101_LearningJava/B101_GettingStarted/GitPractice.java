49bc216a76d27fdef2cbb55c2cf96847b626c8cd
Mehmet Dogan <m.dogan.4123@gmail.com>
2020-10-12 18:20:55 -0400

    Git practice exercise codes

---
+++ src/test/java/A101_LearningJava/B101_GettingStarted/GitPractice.java
@@ -0,0 +1,13 @@
+package A101_LearningJava.B101_GettingStarted;
+
+public class GitPractice {
+
+
+    public static void main(String[] args) {
+        System.out.println(" git practice");
+    }
+
+
+
+
+}
